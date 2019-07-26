e20141167655124d5e6d133a405e58a6ff89209a
zhangli6631 <zhangli6631@navinfo.com>
2019-07-26 11:32:08 +0800

    eureka+Ribbon

--- microservice-consumer-movie/src/main/java/com/tension/cloud/study/controller/MovieController.java
+++ microservice-consumer-movie/src/main/java/com/tension/cloud/study/controller/MovieController.java
@@ -1,7 +1,7 @@
 package com.tension.cloud.study.controller;
 
 import com.tension.cloud.study.entity.User;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +14,16 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/movies")
 @RestController
 public class MovieController {
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+    @Autowired
+    private RestTemplate restTemplate;
+
+//    @GetMapping("/users/{id}")
+//    public User findById(@PathVariable Long id) {
+//        return restTemplate().getForObject("http://localhost:8080/users/{id}", User.class, id);
+//    }
 
     @GetMapping("/users/{id}")
     public User findById(@PathVariable Long id) {
-        return restTemplate().getForObject("http://localhost:8080/users/{id}", User.class, id);
+        return restTemplate.getForObject("http://microservice-provider-user/users/{id}", User.class, id);
     }
 }
